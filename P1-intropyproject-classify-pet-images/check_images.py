2d5c01b31973cc50beb0c9febf10050dd5608dbf
Janay Harris <jha2972@wgu.edu>
2024-07-01 23:32:54 -0500

    TODO 0 edit adding header properly && TODO 1

--- P1-intropyproject-classify-pet-images/check_images.py
+++ P1-intropyproject-classify-pet-images/check_images.py
@@ -3,9 +3,9 @@
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: Janay Harris
+# DATE CREATED:6/29/2024                                  
+# REVISED DATE: 7/1/2024
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
 #          summarizes how well the CNN performed on the image classification task. 
@@ -42,6 +42,8 @@ def main():
     # TODO 0: Measures total program runtime by collecting start time
     start_time = time()
     
+    #sleep(75)
+
     # TODO 1: Define get_input_args function within the file get_input_args.py
     # This function retrieves 3 Command Line Arugments from user as input from
     # the user running the program from a terminal window. This function returns
--- P1-intropyproject-classify-pet-images/get_input_args.py
+++ P1-intropyproject-classify-pet-images/get_input_args.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
+# PROGRAMMER: Janay Harris 
+# DATE CREATED: 6/29/2024                                    
+# REVISED DATE: 7/1/2024
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
 #          provide some or all of the 3 inputs, then the default values are
@@ -38,6 +38,12 @@ def get_input_args():
     Returns:
      parse_args() -data structure that stores the command line arguments object  
     """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dir', type = str, default = 'pet_images/', help = 'path to the image folder')
+    parser.add_argument('--arch', type = str, default = 'vgg', help = 'CNN model architecture')
+    parser.add_argument('--dogfile', type = str, default = 'dognames.txt', help = 'path to the text file with dog names')
+    
     # Replace None with parser.parse_args() parsed argument collection that 
     # you created with this function 
-    return None
+    return parser.parse_args()
